e752321f12e36f80055a6b713eb0add81baca973
Kurt <kurt.bauer@pendo.io>
2022-01-20 13:57:18 -0500

    update addEmailAndIdToTimeEntry function, mutating data correctly

--- middleware/harvestMiddleware.js
+++ middleware/harvestMiddleware.js
@@ -14,7 +14,7 @@ const getTimeEntriesEndpoint = `/v2/time_entries`;
 
 /* HARVEST API URL CONSTRUCTION */
 const getProjectsFromHarvestUrl = process.env.HARVEST_URL + getProjectsEndpoint;
-const getASingleTimeEntryFromHarvestUrl = process.env.HARVEST_URL + getTimeEntriesEndpoint;
+const getAaEmailAndIdFromMondayFromHarvestUrl = process.env.HARVEST_URL + getTimeEntriesEndpoint;
 const getAllAssignedTasksFromHarvestUrl = process.env.HARVEST_URL + getAllAssignedTasksEndpoint;
 const getASingleProjectFromHarvestUrl = process.env.HARVEST_URL + getSingleProjectEndpoint;
 const getProjectUserAssignmentsUrl = process.env.HARVEST_URL + getAllUserAssignmentsEndpoint;
@@ -95,12 +95,12 @@ module.exports = {
         await timer(1000); // Then the created Promise can be awaited
         // Finally the timeout completes & execution continues at this point. 
         console.log(
-        `On Page: ${pageCount}` + ` of ${totalPageCount}` + ' /',
+        `On Page: ${pageCount}` + ` of ${totalPageCount}` + ' -',
         'TimeEntries Stored Total: ' + filteredTimeEntryObjectsBySpecificProjectCode.length,
         );
       }
 
-      console.log(filteredTimeEntryObjectsBySpecificProjectCode, '==============> FILTERING OF TIME ENTRIES DONE');
+      console.log(filteredTimeEntryObjectsBySpecificProjectCode, '============== FILTERING OF TIME ENTRIES DONE ==============');
       res.locals.filteredTimeEntryObjectsBySpecificProjectCode = filteredTimeEntryObjectsBySpecificProjectCode
       return next()
     }
@@ -109,32 +109,17 @@ module.exports = {
     // End of logic for loadAPIRequestsWithDelayTimer()  ------------------------------------------------------------------------------------<
   },
   buildTimeEntriesForMondayBoard: async (req, res, next) => {
-
+    console.log('============================================= buildTimeEntriesForMondayBoard function started =============================================');
     // Pull stored UserProjectAssignment Objects
     let allHarvestTimeEntries = await res.locals.filteredTimeEntryObjectsBySpecificProjectCode
-
-    // To hold filtered time stamps only
-    let filteredUserProjectArrays
-
-    // Create date time object
-    let dateOfToday = new Date();
-
-    // Transform into ISO format, like this => 2022-01-04
-    let todaysDateIsoUtcTimezone = await function ISODateString(datOfToday) {
-      function pad(n) {return n<10 ? '0'+n : n}
-      return datOfToday.getUTCFullYear()+'-'
-          + pad(datOfToday.getUTCMonth()+1)+'-'
-          + pad(datOfToday.getUTCDate())
-    }(dateOfToday)
     
     filteredTimeEntryObjectsForMonday = allHarvestTimeEntries.map((oneTimeEntry)=>{
-      console.log(oneTimeEntry, 'OneTimeEntry <--------');
       // Filter out required information, to communicate with Monday.com
       const mondayTimeEntry = {
         timeEntryId: oneTimeEntry.id,
         submitterId: oneTimeEntry.user.id,
         submitter: oneTimeEntry.user.name,
-        submitterEmail: '',
+        email: '',
         mondayId: 0,
         billableBoolean: oneTimeEntry.billable,
         dateSubmitted: oneTimeEntry.spent_date,
@@ -152,78 +137,102 @@ module.exports = {
 
     // Store locally
     res.locals.filteredTimeEntryObjectsForMonday = filteredTimeEntryObjectsForMonday
-    console.log(filteredTimeEntryObjectsForMonday, '... filteredUserProjectArrays middleware complete', filteredTimeEntryObjectsForMonday.length);
+    console.log(' ======= filteredUserProjectArrays middleware complete ==========', filteredTimeEntryObjectsForMonday.length);
+    return next()
 
-    next()
+    //TODO: Move this date object to a part of the app where it can be better used
+    // // Create date time object
+    // let dateOfToday = new Date();
+
+    // // Transform into ISO format, like this => 2022-01-04
+    // let todaysDateIsoUtcTimezone = await function ISODateString(datOfToday) {
+    //   function pad(n) {return n<10 ? '0'+n : n}
+    //   return datOfToday.getUTCFullYear()+'-'
+    //       + pad(datOfToday.getUTCMonth()+1)+'-'
+    //       + pad(datOfToday.getUTCDate())
+    // }(dateOfToday)
   },
   addEmailAndIdToTimeEntry: async (req, res, next) => {
     // Users{} from Monday.com, contain Email and Id properties 
-    let mondayUserEmailIdObjects = res.locals.allMondayUsersContainer
     // TimeEntries{} from Harvest, contain properties formatted in a previous function - thus unique names
     let filteredTimeEntryObjectsForMonday = res.locals.filteredTimeEntryObjectsForMonday
-
     // Creates array of only Harvest User Ids, required to request specific Users, to then pull email values
-    let harvestUserIdCollection = filteredTimeEntryObjectsForMonday.map((singleTimeEntry)=> {
+    let harvestTimeEntrySubmitterIds = filteredTimeEntryObjectsForMonday.map((singleTimeEntry)=> {
       return singleTimeEntry.submitterId
     })
 
-    // To hold final collection of Objects{}, sanitized to be consumed then be mapped ot Graphql/Monday.com values
-    let completeHarvestObjectWithMondayID = [];
-
-    //TODO: loop through filteredTimeEntryObjectsForMonday and use getAllUsersToFilterIDs, to inpute an email, then match based on that.
-
-    // Note: This function kicks butt  ------------------------------------------------------------------------------------<
+    // Start of logic for loadAPIRequestsWithDelayTimer()  ------------------------------------------------------------------------------------<
     // Returns a Promise that resolves after "ms" Milliseconds
     const timer = milliseconds => new Promise(response => setTimeout(response, milliseconds))
     // Function below will push values to this Array[]
-    let harvestUsersContainer = [];
+    let harvestUserIdNameEmail = [];
 
-      async function loadAPIRequestsWithDelayTimer() { // We need to wrap the loop in a asynchronus function for this to work
-        console.log('Your Harvest copllection of User IDs is this long: ' + harvestUserIdCollection.length );
-        for (var i = 0; i < harvestUserIdCollection.length - 1; harvestUserIdCollection[i++]) {
-          // Injects Specified User Ids into URL, to request their User Profiles
-          let paginationUrl = `https://api.harvestapp.com/v2/users/${harvestUserIdCollection[i]}`
+    console.log('Your Harvest collection of User IDs is this long: ' + harvestTimeEntrySubmitterIds.length );
 
-          console.log(i);
+    async function loadAPIRequestsWithDelayTimer() { // We need to wrap the loop in a asynchronus function for this to work
+        
+      for (var i = 0; i <= harvestTimeEntrySubmitterIds.length - 1; harvestTimeEntrySubmitterIds[i++]) {
+        // Injects Specified User Ids into URL, to request their User Profiles
+        let paginationUrl = `https://api.harvestapp.com/v2/users/${harvestTimeEntrySubmitterIds[i]}`
 
-          // Send GET request to specific URL Endpoint, dynamically defined above
-          axios.get(paginationUrl, axiosConfigObject)
-          .then((response)=>{
-            // Push to me array, defined outside of async function
-            harvestUsersContainer.push(response.data)
-          })
+        console.log(`Currently on User Profile Number: ${i} of ${harvestTimeEntrySubmitterIds.length - 1}`);
 
-          // When the engine reaches the await part, it sets a timeout and halts the execution of the async function.
-          await timer(1000); // Then the created Promise can be awaited
-          // Finally the timeout completes & execution continues at this point. 
-        }
+        // Send GET request to specific URL Endpoint, dynamically defined above
+        axios.get(paginationUrl, axiosConfigObject)
+        .then((mySpecificHarvestUser)=>{
+  
+          mySpecificHarvestUser.data
 
-        // Map over list of all Harvest Users, to match based on TimeEntryUserId And HarvestUserId
-        harvestUsersContainer.map((singleHarvestUserProfile)=>{
-          
-          // TODO: get TimeEntryUserIds, use to match with Harvest User Ids and add email
+          filertedHarvestUserData = {
+            id: mySpecificHarvestUser.data.id,
+            first_name: mySpecificHarvestUser.data.first_name,
+            last_name: mySpecificHarvestUser.data.last_name,
+            email: mySpecificHarvestUser.data.email
+          }
 
-          // Map over singleTimeEntry{}, an object which has an empty string set to the property singleTimeEntry.submitterEmail
-          // Information needs to be added from a User{}, which is requested in the async function above.
-          filteredTimeEntryObjectsForMonday.map((singleTimeEntry)=> {
-            // If a HarvestTimeEntry and a HarvestUserProfile ahve the same ID
-            // Then set a submitterEmail
-            // And set a Monday ID in my HarvestTimeEntry Object
-            if(singleTimeEntry.submitterId === singleHarvestUserProfile.id){
-              singleTimeEntry.submitterEmail = singleHarvestUserProfile.email 
-              singleTimeEntry.mondayId = singleHarvestUserProfile.id
-            }
-          })
+          harvestUserIdNameEmail.push(filertedHarvestUserData)            
+        })
+
+        // When the engine reaches the await part, it sets a timeout and halts the execution of the async function.
+        await timer(1000); // Then the created Promise can be awaited
+        // Finally the timeout completes & execution continues at this point. 
+      }
+
+      let mondayUserEmailIdObjects = res.locals.allMondayUsersContainer
+      let holdMeTimeEntriesWithEmail = [];
+      filteredTimeEntryObjectsForMonday.map((singleTimeEntry)=>{
+
+        harvestUserIdNameEmail.map((singleHarvestUser)=>{
+
+          if (singleHarvestUser.id === singleTimeEntry.submitterId){
+            singleTimeEntry.email = singleHarvestUser.email;
+            holdMeTimeEntriesWithEmail.push(singleTimeEntry);
+          }
 
         })
 
-        // Finally, set TimeEntry Object form Harvest locally, with Emails & Ids added
-        res.locals.filteredTimeEntryObjectsForMondayWithUserEmail = filteredTimeEntryObjectsForMonday
-        return next()
-      }
-      // Note: End of butt kick funcrtion here  ------------------------------------------------------------------------------------<
+      })
 
-      loadAPIRequestsWithDelayTimer();
+      let uniqueTimeEntries = [];
+      holdMeTimeEntriesWithEmail.filter(function(o1){
+        let i = uniqueTimeEntries.findIndex(x => (x.timeEntryId === o1.timeEntryId));
+        if(i <= -1){
+          uniqueTimeEntries.push(o1);
+        }
+        return null;
+      });
+
+
+      let TimeEntriesWithEmailAndMondayID = uniqueTimeEntries.filter(o1 => mondayUserEmailIdObjects.some(o2 => o1.email === o2.email? o1.mondayId = o2.id : null));
+
+      res.locals.filteredTimeEntryObjectsForMondayWithUserEmail = TimeEntriesWithEmailAndMondayID
+      console.log(`==================================== ${TimeEntriesWithEmailAndMondayID.length} Time Entries Updated ! ====================================`);
+      //TODO: Maybe return here?
+      next()
+    }
+    loadAPIRequestsWithDelayTimer();
+    // End of logic for loadAPIRequestsWithDelayTimer()  ------------------------------------------------------------------------------------<
+
 
   }
 }

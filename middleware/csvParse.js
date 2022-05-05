5d1e7bf0ce2dba6cb0f257a2cd9172cb833dcfd7
Kurt <kurt.bauer@pendo.io>
2022-05-05 10:06:06 -0400

    parse csv and output values

--- middleware/csvParse.js
+++ middleware/csvParse.js
@@ -9,15 +9,58 @@
 
 Object.assign(module.exports, {
   // Express expects a Middleware function in order to run without failing.
-  parseCSV: (req, res, next) =>{
-    const assert = require('assert');
+  parseCSV: async (req, res, next) =>{
+
     const fs = require('fs');
-    const os = require('os')
     const { parse } = require('csv-parse');
+
+    // Below not used, may remove
+    const assert = require('assert');
+    const os = require('os')
+    const path = require('path');
+    const debug = require('debug')('app:csv:service');
+    const chalk = await import('chalk');
+  
+
     // Note, the `stream/promises` module is only available
     // starting with Node.js version 16
     const { finished } = require('stream/promises');
+    
+    // CSV file that is being uploaded
+    const csvFile = './csvFiles/2022_05_04_project-roll-up-board.csv'
 
+    const records = await [];
+    const stream = fs.createReadStream(csvFile);
+    const parser = stream.pipe(parse({ delimiter: ',', columns: true,}));
+
+    parser.on('readable', () => {
+      let record;
+      while ((record = parser.read()) !== null) {
+        records.push(record);
+      }
+    });
+
+    await finished(parser);
+
+    // Create a copy of Records
+    let allMyRecords = records
+
+    allMyRecords.map((aSingleRecord)=>{
+
+      for (const property in aSingleRecord) {
+
+        // String types
+        const myKey = property
+        const myValue = aSingleRecord[property]
+  
+        console.log(`${myKey}: ${myValue}`);
+  
+      }
+
+    })
+
+    // TODO: Uncomment once a push to Monday.com is ready
+    // return records;
     next()
   }
 

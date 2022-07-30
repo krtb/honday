fd6957f30cbe98fa14f532e35aab98692cb69308
Kurt <kurt.bauer@pendo.io>
2022-07-30 08:49:50 -0400

    modify JSDoc comment to better describe function

--- utils/parseCSV.js
+++ utils/parseCSV.js
@@ -3,15 +3,22 @@ let { parse } = require('csv-parse');
 /** `stream/promises` Node version must be 16^ */
 let { finished } = require('stream/promises'); 
 
-// Quirks with using module.exports, when modules circularly depend on each other.
-// It is recommended against replacing the object.
-// For multiple exports at once, using object literal definition, implement the below
-// The exports object is created for your module before your module runs, 
-// and if there are circular dependencies, other modules may have access to that default object before your module can fill it in. 
-// If you replace it, they may have the old, original object, and not (eventually) see your exports. 
-// If you add to it, then even though the object didn't have your exports initially, eventually it will have it, even if the other module got access to the object before those exports existed.
-// https://nodejs.org/api/modules.html#modules_cycles
-
+/**
+ * Quirks with using module.exports, when modules circularly depend on each other.
+ * It is recommended against replacing the object.
+ * For multiple exports at once, using object literal definition, implement the below
+ * The exports object is created for your module before your module runs, 
+ * and if there are circular dependencies, other modules may have access to that default object before your module can fill it in. 
+ * If you replace it, they may have the old, original object, and not (eventually) see your exports. 
+ * If you add to it, then even though the object didn't have your exports initially, 
+ * eventually it will have it, even if the other module got access to the object before those exports existed.
+ * https://nodejs.org/api/modules.html#modules_cycles
+ * @method Object.assign - method copies all enumerable own properties from one or more source objects to a target object;
+ * it assigns properties, versus copying or defining new properties. 
+ * @param {object} module.exports - Target Object property, the Node Module object.
+ * What to apply the sources' properties to, which is returned after it is modified.
+ * @param {objects} {} - Source objects containing properties that should be applied to our Target object.
+ */
 Object.assign(module.exports, 
   { 
     /**

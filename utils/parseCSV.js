5f4cb0c1f391288f8868402bb00cd19261ee58d5
Kurt <kurt.bauer@pendo.io>
2022-07-30 06:04:12 -0400

    add dynamic utility function to parse csv files

---
+++ utils/parseCSV.js
@@ -0,0 +1,44 @@
+let fs = require('fs');
+let { parse } = require('csv-parse');
+/** `stream/promises` Node version must be 16^ */
+let { finished } = require('stream/promises'); 
+
+// Quirks with using module.exports, when modules circularly depend on each other.
+// It is recommended against replacing the object.
+// For multiple exports at once, using object literal definition, implement the below
+// The exports object is created for your module before your module runs, 
+// and if there are circular dependencies, other modules may have access to that default object before your module can fill it in. 
+// If you replace it, they may have the old, original object, and not (eventually) see your exports. 
+// If you add to it, then even though the object didn't have your exports initially, eventually it will have it, even if the other module got access to the object before those exports existed.
+// https://nodejs.org/api/modules.html#modules_cycles
+
+Object.assign(module.exports, 
+  { 
+    /**
+      * Delay requests made to an API Endpoint in order avoid throttling by server.
+      * @param {string} csvFileLocation - File location of your CSV file.
+      * @returns {(array|objects)} records - Returns array of objects, with properties and values from CSV file.
+      */
+    parseCsvFileToData: async function parseCsvFileToData(csvFileLocation) {
+      
+      let myFilePathCopy = typeof csvFileLocation === 'string'? csvFileLocation : 'Missing Your CSV File.'
+
+      const csvFilePath = myFilePathCopy
+      const stream = fs.createReadStream(csvFilePath);
+      const parser = stream.pipe(parse({ delimiter: ',', columns: true,}));
+
+      let records = [];
+
+      parser.on('readable', () => {
+        let record;
+        while ((record = parser.read()) !== null) {
+          records.push(record);
+        };
+      });
+
+      await finished(parser);
+      return records
+
+    }
+  }
+);
